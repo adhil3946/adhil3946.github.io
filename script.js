0e01b368937c62792d351bf8df23f3a062193cbc
adhil3946 <mohdadhil086@gmail.com>
2025-10-30 16:57:54 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-$(document).ready(function(){
- $('.navbar-toggle').click(function(){
-    $(this).toggleClass('active')
- })
- $('.navbar-toggle').click(function(){
-    $('.nav-menu').toggleClass('active')
- })
+$(document).ready(function () {
+   $('.navbar-toggle').click(function () {
+      $(this).toggleClass('active')
+   })
+   $('.navbar-toggle').click(function () {
+      $('.nav-menu').toggleClass('active')
+   })
 });
+$(document).ready(function () {
+   // Show or hide the button
+   $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {
+         $('.scroll-up').fadeIn();
+      } else {
+         $('.scroll-up').fadeOut();
+      }
+   });
+   // Scroll to top when button clicked
+   $('.scroll-up').click(function () {
+      $('html, body').animate({ scrollTop: 0 }, 800);
+      return false;
+   });
+})
